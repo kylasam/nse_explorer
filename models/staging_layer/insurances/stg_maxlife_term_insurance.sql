e7181d4c5cdbc5e7deb87d1076f9d39f1828214f
Kylasam NA <kylasam.arruchamy+bm@bringmeister.de>
2023-11-27 23:10:07 +0000

    1-set up commit for Investments

---
+++ models/staging_layer/insurances/stg_maxlife_term_insurance.sql
@@ -0,0 +1,21 @@
+select 
+       6 as investment_code,
+       110 as src_sys_id,
+       md.month_start_date, 
+       322922519 as policy_number,
+       'Max Life Online Term Plan Plus' as plan_name,
+       coalesce(sc.premium_amount, 0) as premium_amount,
+       current_timestamp() as edw_publn_id
+from {{ source("MONTHLY_EXPENSES_SOURCE", "dates_mnthly") }} md
+left join
+    (
+        select 
+            dates as premium_dates, 
+            15000 as premium_amount
+        from
+            unnest(
+                generate_date_array('2016-03-01', '2065-05-01', interval 12 month)
+            ) dates
+    ) sc
+    on md.month_start_date = sc.premium_dates
+order by 2
