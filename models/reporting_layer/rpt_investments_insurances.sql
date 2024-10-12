7fcdb35a6f5065dcfeca6d02f5cad38b6dea3ad5
Kylasam NA <42716803+kylasam@users.noreply.github.com>
2024-10-12 12:54:11 +0200

    Update rpt_investments_insurances.sql

--- models/reporting_layer/rpt_investments_insurances.sql
+++ models/reporting_layer/rpt_investments_insurances.sql
@@ -1,52 +1,48 @@
-SELECT
+select
     start_dt,
     premium_amount,
     insurer_type,
     end_dt
-FROM (
-    SELECT 
-        MIN(month_start_date) AS start_dt,
-        SUM(amount) AS premium_amount,
-        'edelweisstokio' AS insurer_type,
-        MAX(month_start_date) AS end_dt
-    FROM
-        {{ ref("stg_investments_edelweisstokio") }}
-    WHERE amount > 0
-    GROUP BY insurer_type
+from (
+    select 
+        min(month_start_date) as start_dt,
+        sum(amount) as premium_amount,
+        'edelweisstokio' as insurer_type,
+        max(month_start_date) as end_dt
+    from {{ ref("stg_investments_edelweisstokio") }}
+    where amount > 0
+    group by insurer_type
 
     UNION ALL
 
-    SELECT 
-        MIN(month_start_date) AS start_dt,
-        SUM(amount) AS premium_amount,
-        'lic' AS insurer_type,
-        MAX(month_start_date) AS end_dt
-    FROM
-        {{ ref("stg_investments_lic") }}
-    WHERE amount > 0
-    GROUP BY insurer_type
+    select 
+        min(month_start_date) as start_dt,
+        sum(amount) as premium_amount,
+        'lic' as insurer_type,
+        max(month_start_date) as end_dt
+    from {{ ref("stg_investments_lic") }}
+    where amount > 0
+    group by insurer_type
 
     UNION ALL
 
-    SELECT 
-        MIN(month_start_date) AS start_dt,
-        SUM(amount) AS premium_amount,
-        'aviva' AS insurer_type,
-        MAX(month_start_date) AS end_dt
-    FROM
-        {{ ref("stg_investments_aviva") }}
-    WHERE amount > 0
-    GROUP BY insurer_type
+    select 
+        min(month_start_date) as start_dt,
+        sum(amount) as premium_amount,
+        'aviva' as insurer_type,
+        max(month_start_date) as end_dt
+    from {{ ref("stg_investments_aviva") }}
+    where amount > 0
+    group by insurer_type
 
     UNION ALL
 
-    SELECT 
-        MIN(month_start_date) AS start_dt,
-        SUM(amount) AS premium_amount,
-        'pnbmetlife' AS insurer_type,
-        MAX(month_start_date) AS end_dt
-    FROM
-        {{ ref("stg_investments_pnbmetlife") }}
-    WHERE amount > 0
-    GROUP BY insurer_type
-) AS combined_results;  -- Added an alias for the inner query
+    select 
+        min(month_start_date) as start_dt,
+        sum(amount) as premium_amount,
+        'pnbmetlife' as insurer_type,
+        max(month_start_date) as end_dt
+    from {{ ref("stg_investments_pnbmetlife") }}
+    where amount > 0
+    group by insurer_type
+)
