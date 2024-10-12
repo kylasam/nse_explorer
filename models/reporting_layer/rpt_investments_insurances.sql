720c38e305a81952539c491fddcb28ef39541ee1
Kylasam NA <42716803+kylasam@users.noreply.github.com>
2024-10-12 12:18:24 +0200

    Update rpt_investments_insurances.sql

--- models/reporting_layer/rpt_investments_insurances.sql
+++ models/reporting_layer/rpt_investments_insurances.sql
@@ -3,12 +3,12 @@ select
     premium_amount,
     insurer_type,
     end_dt
-    from(
+    from (
 select 
         min(month_start_date) as start_dt,
         sum(amount) as premium_amount,
         'edelweisstokio' as insurer_type,
-        max(month_start_date) as end_dt,
+        max(month_start_date) as end_dt
 
 FROM
 {{ ref("stg_investments_edelweisstokio") }}
@@ -19,29 +19,29 @@ select
         min(month_start_date) as start_dt,
         sum(amount) as premium_amount,
         'lic' as insurer_type,
-        max(month_start_date) as end_dt,
+        max(month_start_date) as end_dt
 
 FROM
 {{ ref("stg_investments_lic") }}
 where amount > 0
-group by 3)
+group by 3
 UNION ALL
 select 
         min(month_start_date) as start_dt,
         sum(amount) as premium_amount,
         'aviva' as insurer_type,
-        max(month_start_date) as end_dt,
+        max(month_start_date) as end_dt
 
 FROM
 {{ ref("stg_investments_aviva") }}
 where amount > 0
-group by 3)
+group by 3
 UNION ALL
 select 
         min(month_start_date) as start_dt,
         sum(amount) as premium_amount,
         'pnbmetlife' as insurer_type,
-        max(month_start_date) as end_dt,
+        max(month_start_date) as end_dt
 
 FROM
 {{ ref("stg_investments_pnbmetlife") }}
